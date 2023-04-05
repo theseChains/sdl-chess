33750a08928eb824252748848482214c9477da05
theseChains <rasim0103@gmail.com>
2023-04-05 22:55:56 +0300

    Render an 8x8 board of rectangles

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Constants.h"
 #include "Window.h"
 
+#include <array>
 #include <iostream>
 
 int main()
@@ -8,7 +9,10 @@ int main()
     Window window{ constants::windowWidth, constants::windowHeight };
     SDL_Renderer* renderer{ SDL_CreateRenderer(window.getWindow(), -1, SDL_RENDERER_SOFTWARE) };
 
-    SDL_Rect rectangle{ 0, 0, 100, 100 };
+    std::array<std::array<SDL_Rect, 8>, 8> board{};
+    for (int i{ 0 }; i < 8; ++i)
+        for (int j{ 0 }; j < 8; ++j)
+            board[i][j] = { i * 100, j * 100, 100, 100 };
 
     bool run{ true };
     SDL_Event event{};
@@ -19,12 +23,17 @@ int main()
             if (event.type == SDL_QUIT)
                 run = false;
 
-            SDL_SetRenderDrawColor(renderer, 120, 120, 230, 255);
             SDL_RenderClear(renderer);
 
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-            SDL_RenderFillRect(renderer, &rectangle);
-            SDL_RenderDrawRect(renderer, &rectangle);
+            for (unsigned char i{ 0 }; i < 8; ++i)
+            {
+                for (unsigned char j{ 0 }; j < 8; ++j)
+                {
+                    SDL_SetRenderDrawColor(renderer, i * 32, j * 32, 255, 255);
+                    SDL_RenderFillRect(renderer, &board[i][j]);
+                    SDL_RenderDrawRect(renderer, &board[i][j]);
+                }
+            }
 
             SDL_RenderPresent(renderer);
         }
