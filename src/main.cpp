7a259c002afe001da27205b24f30486501096a5f
theseChains <rasim0103@gmail.com>
2023-04-05 22:44:22 +0300

    Render a rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 int main()
 {
     Window window{ constants::windowWidth, constants::windowHeight };
+    SDL_Renderer* renderer{ SDL_CreateRenderer(window.getWindow(), -1, SDL_RENDERER_SOFTWARE) };
+
+    SDL_Rect rectangle{ 0, 0, 100, 100 };
 
     bool run{ true };
     SDL_Event event{};
@@ -15,6 +18,15 @@ int main()
         {
             if (event.type == SDL_QUIT)
                 run = false;
+
+            SDL_SetRenderDrawColor(renderer, 120, 120, 230, 255);
+            SDL_RenderClear(renderer);
+
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderFillRect(renderer, &rectangle);
+            SDL_RenderDrawRect(renderer, &rectangle);
+
+            SDL_RenderPresent(renderer);
         }
     }
 
