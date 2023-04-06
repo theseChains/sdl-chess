71a0cf9cbb3d5fb4f21f356a77c516cdb8f364a5
theseChains <rasim0103@gmail.com>
2023-04-06 18:42:03 +0300

    Replace the sdl renderer function calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Constants.h"
+#include "Renderer.h"
 #include "Window.h"
 
 #include <SDL2/SDL_image.h>
@@ -9,14 +10,14 @@
 int main()
 {
     Window window{ constants::windowWidth, constants::windowHeight };
-    SDL_Renderer* renderer{ SDL_CreateRenderer(window.getWindow(), -1, SDL_RENDERER_SOFTWARE) };
+    Renderer renderer{ window.getWindow(), -1, SDL_RENDERER_SOFTWARE };
 
     std::array<std::array<SDL_Rect, 8>, 8> board{};
     for (int i{ 0 }; i < 8; ++i)
         for (int j{ 0 }; j < 8; ++j)
             board[i][j] = { i * 100, j * 100, 100, 100 };
 
-    SDL_Texture* image{ IMG_LoadTexture(renderer, "../res/pawn.png") };
+    SDL_Texture* image{ renderer.loadTexture("../res/knight.png") };
     int imageWidth{};
     int imageHeight{};
     SDL_QueryTexture(image, nullptr, nullptr, &imageWidth, &imageHeight);
@@ -34,27 +35,29 @@ int main()
         {
             if (event.type == SDL_QUIT)
                 run = false;
+
+            if (event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_ESCAPE)
+                run = false;
         }
 
-        SDL_RenderClear(renderer);
+        renderer.clear();
 
         for (unsigned char i{ 0 }; i < 8; ++i)
         {
             for (unsigned char j{ 0 }; j < 8; ++j)
             {
                 if ((i + j) % 2 == 0)
-                    SDL_SetRenderDrawColor(renderer, 230, 230, 230, 255);
+                    renderer.setDrawColor(230, 230, 230, 255);
                 else
-                    SDL_SetRenderDrawColor(renderer, 150, 75, 0, 255);
+                    renderer.setDrawColor(150, 75, 0, 255);
 
-                SDL_RenderFillRect(renderer, &board[i][j]);
-                SDL_RenderDrawRect(renderer, &board[i][j]);
+                renderer.fillAndDrawRect(board[i][j]);
 
-                SDL_RenderCopy(renderer, image, nullptr, &pieces[i][j]);
+                renderer.copyTexture(image, nullptr, &pieces[i][j]);
             }
         }
 
-        SDL_RenderPresent(renderer);
+        renderer.present();
     }
 
     return 0;
