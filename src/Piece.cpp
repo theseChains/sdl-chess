c2ec7949c7f68957c0549cb3cbc4a12f7a67b0a7
theseChains <rasim0103@gmail.com>
2023-04-07 00:42:32 +0300

    Add the piece draw function

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -1,5 +1,13 @@
 #include "Piece.h"
 
-Piece::Piece(PieceType type) : m_type{ type }
+Piece::Piece(PieceType type, SDL_Texture* texture, std::pair<int, int> position)
+    : m_type{ type }, m_texture{ texture }, m_position{ position }
 {
+    // todo: replace 100's with constants
+    m_rectangle = { position.first, position.second, 100, 100 };
+}
+
+void Piece::draw(Renderer& renderer)
+{
+    renderer.copyTexture(m_texture, nullptr, &m_rectangle);
 }
--- src/Piece.h
+++ src/Piece.h
@@ -2,14 +2,24 @@
 #define PIECE_H
 
 #include "PieceType.h"
+#include "Renderer.h"
+
+#include <SDL2/SDL_image.h>
+
+#include <tuple>
 
 class Piece
 {
 public:
-    Piece(PieceType type);
+    Piece(PieceType type, SDL_Texture* texture, std::pair<int, int> position);
+
+    void draw(Renderer& renderer);
 
 private:
     PieceType m_type{};
+    SDL_Rect m_rectangle{};
+    SDL_Texture* m_texture{};
+    std::pair<int, int> m_position{};
 };
 
 #endif
