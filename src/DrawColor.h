7e3fad4da4c6050a926f8651c55cf145a1b96af5
theseChains <rasim0103@gmail.com>
2023-04-06 22:04:28 +0300

    Add a draw color struct

---
+++ src/DrawColor.h
@@ -0,0 +1,12 @@
+#ifndef DRAW_COLOR_H
+#define DRAW_COLOR_H
+
+struct DrawColor
+{
+    unsigned char red{};
+    unsigned char green{};
+    unsigned char blue{};
+    unsigned char alpha{ 255 };
+};
+
+#endif
