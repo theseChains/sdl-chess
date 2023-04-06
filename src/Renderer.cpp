41c147de548e5d701fcc2a670626d3057c3a0922
theseChains <rasim0103@gmail.com>
2023-04-06 18:41:23 +0300

    Make a renderer wrapper

---
+++ src/Renderer.cpp
@@ -0,0 +1,54 @@
+#include "Renderer.h"
+
+Renderer::Renderer(SDL_Window* window, int index, unsigned int flags)
+{
+    m_renderer = SDL_CreateRenderer(window, index, flags);
+}
+
+void Renderer::clear()
+{
+    SDL_RenderClear(m_renderer);
+}
+
+void Renderer::present()
+{
+    SDL_RenderPresent(m_renderer);
+}
+
+void Renderer::setDrawColor(unsigned char red, unsigned char green, unsigned char blue,
+        unsigned char alpha)
+{
+    SDL_SetRenderDrawColor(m_renderer, red, green, blue, alpha);
+}
+
+void Renderer::fillRect(const SDL_Rect& rectangle)
+{
+    SDL_RenderFillRect(m_renderer, &rectangle);
+}
+
+void Renderer::drawRect(const SDL_Rect& rectangle)
+{
+    SDL_RenderDrawRect(m_renderer, &rectangle);
+}
+
+void Renderer::fillAndDrawRect(const SDL_Rect& rectangle)
+{
+    fillRect(rectangle);
+    drawRect(rectangle);
+}
+
+SDL_Texture* Renderer::loadTexture(const std::string& path)
+{
+    return IMG_LoadTexture(m_renderer, path.data());
+}
+
+void Renderer::copyTexture(SDL_Texture* texture, const SDL_Rect* source,
+        const SDL_Rect* destination)
+{
+    SDL_RenderCopy(m_renderer, texture, source, destination);
+}
+
+Renderer::~Renderer()
+{
+    SDL_DestroyRenderer(m_renderer);
+}
---
+++ src/Renderer.h
@@ -0,0 +1,34 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <string>
+
+class Renderer
+{
+public:
+    Renderer(SDL_Window* window, int index, unsigned int flags);
+
+    void clear();
+    void present();
+
+    void setDrawColor(unsigned char red, unsigned char green, unsigned char blue,
+            unsigned char alpha);
+
+    void fillRect(const SDL_Rect& rectangle);
+    void drawRect(const SDL_Rect& rectangle);
+    void fillAndDrawRect(const SDL_Rect& rectangle);
+
+    SDL_Texture* loadTexture(const std::string& path);
+    // source and destination can be null, hence pass by pointer
+    void copyTexture(SDL_Texture* texture, const SDL_Rect* source, const SDL_Rect* destination);
+
+    ~Renderer();
+
+private:
+    SDL_Renderer* m_renderer{};
+};
+
+#endif
