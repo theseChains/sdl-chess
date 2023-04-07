5e2a0dea73f8b70916fc643ad8bc88271252adfa
theseChains <rasim0103@gmail.com>
2023-04-07 20:49:41 +0300

    Implement a hash function for the piece type and color pair

--- src/TextureTable.h
+++ src/TextureTable.h
@@ -1,6 +1,7 @@
 #ifndef TEXTURE_TABLE_H
 #define TEXTURE_TABLE_H
 
+#include "Colors.h"
 #include "PieceType.h"
 #include "Renderer.h"
 
@@ -12,10 +13,18 @@ class TextureTable
 {
 public:
     TextureTable(Renderer& renderer);
-    SDL_Texture* operator[](PieceType type);
+    SDL_Texture* operator[](const std::pair<PieceColor, PieceType>& pieceInfo);
 
 private:
-    std::unordered_map<PieceType, SDL_Texture*> m_table{};
+    struct PairHash
+    {
+        std::size_t operator()(const std::pair<PieceColor, PieceType>& pair) const
+        {
+            return std::hash<PieceColor>()(pair.first) ^ std::hash<PieceType>()(pair.second);
+        }
+    };
+
+    std::unordered_map<std::pair<PieceColor, PieceType>, SDL_Texture*, PairHash> m_table{};
 };
 
 #endif
