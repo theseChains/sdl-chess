3fdfc4a3deacbd84d4c7835230c4f0cb8281abb3
theseChains <rasim0103@gmail.com>
2023-04-09 18:57:02 +0300

    Add pawn position constants

--- src/Constants.h
+++ src/Constants.h
@@ -6,6 +6,10 @@ namespace constants
     inline constexpr int windowWidth{ 800 };
     inline constexpr int windowHeight{ 800 };
     inline constexpr int boardSize{ 8 };
+
+    // rows and columns go from 0 to 7
+    inline constexpr int whitePawnStartRow{ 6 };
+    inline constexpr int blackPawnStartRow{ 1 };
 }
 
 #endif
