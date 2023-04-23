b0661e3994b090250e32617d8229d7b4af7b085f
theseChains <rasim0103@gmail.com>
2023-04-23 16:54:27 +0300

    Remove unused function declaration

--- src/KingCheckLogic.h
+++ src/KingCheckLogic.h
@@ -9,8 +9,6 @@
 std::optional<std::pair<int, int>> findKingPosition(const std::array<std::array<Tile, 8>, 8>& board,
         PieceColor kingColor);
 
-bool rookOrQueenAttacksKing(const std::array<std::array<Tile, 8>, 8>& board, int kingRow,
-        int kingColumn, PieceColor kingColor);
 bool isKingInCheck(const std::array<std::array<Tile, 8>, 8>& board, PieceColor kingColor);
 // pass a copy of the board to see if the king will be in check after moving a piece to a new spot
 bool kingWillBeInCheck(std::array<std::array<Tile, 8>, 8> board, const Piece& piece,
