80b99c19f626dd7a13e577cc7a682ce82787a7c8
theseChains <rasim0103@gmail.com>
2023-07-06 16:52:53 +0300

    Implement the mate logic

--- src/KingMateLogic.cpp
+++ src/KingMateLogic.cpp
@@ -3,12 +3,9 @@
 
 #include <iostream>
 
-bool doesKingHaveValidMoves(const std::array<std::array<Tile, 8>, 8>& board, PieceColor kingColor)
+bool kingHasLegalMoves(const std::array<std::array<Tile, 8>, 8>& board, const Piece& king,
+        int currentRow, int currentColumn)
 {
-    auto [kingRow, kingColumn]{ findKingPosition(board, kingColor).value() };
-    Piece king{ board[kingRow][kingColumn].getPiece().value() };
-    // this doesn't print anything at all.. nonsense
-
     for (int rowOffset{ -1 }; rowOffset < 2; ++rowOffset)
     {
         for (int columnOffset{ -1 }; columnOffset < 2; ++columnOffset)
@@ -16,31 +13,151 @@ bool doesKingHaveValidMoves(const std::array<std::array<Tile, 8>, 8>& board, Pie
             if (rowOffset == 0 && columnOffset == 0)
                 continue;
 
-            int newRow{ kingRow + rowOffset };
-            int newColumn{ kingColumn + columnOffset };
+            int newRow{ currentRow + rowOffset };
+            int newColumn{ currentColumn + columnOffset };
 
             if (newRow > 7 || newRow < 0 || newColumn > 7 || newColumn < 0)
                 continue;
 
             if (MoveValidator::moveIsValid(board, king, newRow, newColumn) &&
                     !kingWillBeInCheck(board, king, newRow, newColumn))
-            {
-                std::cout << "king has valid move at " << newRow << ' ' << newColumn << '\n';
                 return true;
-            }
         }
     }
 
-    std::cout << "king has no valid moves\n";
+    return false;
+}
+
+bool pawnHasLegalMoves(const std::array<std::array<Tile, 8>, 8>& board, const Piece& pawn,
+        int currentRow, int currentColumn)
+{
+    for (int columnOffset{ -1 }; columnOffset <= 1; ++columnOffset)
+    {
+        int newRow{ currentRow };
+        if (pawn.getColor() == PieceColor::white)
+            --newRow;
+        else
+            ++newRow;
+        int newColumn{ currentColumn + columnOffset };
+
+        if (newRow > 7 || newRow < 0 || newColumn > 7 || newColumn < 0)
+            continue;
+
+        if (MoveValidator::moveIsValid(board, pawn, newRow, newColumn) &&
+                !kingWillBeInCheck(board, pawn, newRow, newColumn))
+            return true;
+    }
+
+    return false;
+}
+
+bool knightHasLegalMoves(const std::array<std::array<Tile, 8>, 8>& board, const Piece& knight,
+        int currentRow, int currentColumn)
+{
+    std::array<int, 8> rowOffsets{ -2, -2, -1, -1, 1, 1, 2, 2 };
+    std::array<int, 8> columnOffsets{ 1, -1, 2, -2, 2, -2, 1, -1 };
+    for (int i{ 0 }; i < 8; ++i)
+    {
+        int newRow{ currentRow + rowOffsets[i] };
+        int newColumn{ currentColumn + columnOffsets[i] };
+
+        if (newRow > 7 || newRow < 0 || newColumn > 7 || newColumn < 0)
+            continue;
+
+        if (MoveValidator::moveIsValid(board, knight, newRow, newColumn) &&
+                !kingWillBeInCheck(board, knight, newRow, newColumn))
+            return true;
+    }
+
+    return false;
+}
+
+bool bishopHasLegalMoves(const std::array<std::array<Tile, 8>, 8>& board, const Piece& bishop,
+        int currentRow, int currentColumn)
+{
+    std::array<int, 4> rowDirections{ 1, 1, -1, -1 };
+    std::array<int, 4> columnDirections{ 1, -1, 1, -1 };
+
+    for (int i{ 0 }; i < 4; ++i)
+    {
+        int newRow{ currentRow + rowDirections[i] };
+        int newColumn{ currentColumn + columnDirections[i] };
+
+        while (newRow >= 0 && newRow < 8 && newColumn >= 0 && newColumn < 8)
+        {
+            if (MoveValidator::moveIsValid(board, bishop, newRow, newColumn) &&
+                    !kingWillBeInCheck(board, bishop, newRow, newColumn))
+                return true;
+
+            newRow += rowDirections[i];
+            newColumn += columnDirections[i];
+        }
+    }
+
+    return false;
+}
+
+bool rookHasLegalMoves(const std::array<std::array<Tile, 8>, 8>& board, const Piece& rook,
+        int currentRow, int currentColumn)
+{
+    std::array<int, 4> rowDirections{ 1, -1, 0, 0 };
+    std::array<int, 4> columnDirections{ 0, 0, 1, -1 };
+
+    for (int i{ 0 }; i < 4; ++i)
+    {
+        int newRow{ currentRow + rowDirections[i] };
+        int newColumn{ currentColumn + columnDirections[i] };
+
+        while (newRow >= 0 && newRow < 8 && newColumn >= 0 && newColumn < 8)
+        {
+            if (MoveValidator::moveIsValid(board, rook, newRow, newColumn) &&
+                    !kingWillBeInCheck(board, rook, newRow, newColumn))
+                return true;
+
+            newRow += rowDirections[i];
+            newColumn += columnDirections[i];
+        }
+    }
+
+    return false;
+}
+
+bool playerHasLegalMoves(const std::array<std::array<Tile, 8>, 8>& board, PieceColor color)
+{
+    for (const auto& row : board)
+    {
+        for (const auto& tile : row)
+        {
+            auto piece{ tile.getPiece() };
+            auto [pieceRow, pieceColumn]{ piece->getBoardPosition() };
+            if (piece && piece->getColor() == color && piece->getType() == PieceType::pawn &&
+                    pawnHasLegalMoves(board, piece.value(), pieceRow, pieceColumn))
+                return true;
+            else if (piece && piece->getColor() == color && piece->getType() == PieceType::knight &&
+                    knightHasLegalMoves(board, piece.value(), pieceRow, pieceColumn))
+                return true;
+            else if (piece && piece->getColor() == color && piece->getType() == PieceType::bishop &&
+                    bishopHasLegalMoves(board, piece.value(), pieceRow, pieceColumn))
+                return true;
+            else if (piece && piece->getColor() == color && piece->getType() == PieceType::rook &&
+                    rookHasLegalMoves(board, piece.value(), pieceRow, pieceColumn))
+                return true;
+            else if (piece && piece->getColor() == color && piece->getType() == PieceType::queen &&
+                    rookHasLegalMoves(board, piece.value(), pieceRow, pieceColumn) &&
+                    bishopHasLegalMoves(board, piece.value(), pieceRow, pieceColumn))
+                return true;
+            else if (piece && piece->getColor() == color && piece->getType() == PieceType::king &&
+                    kingHasLegalMoves(board, piece.value(), pieceRow, pieceColumn))
+                return true;
+        }
+    }
 
     return false;
 }
 
 bool isKingCheckmated(const std::array<std::array<Tile, 8>, 8>& board, PieceColor kingColor)
 {
-    // so we need to check if any pieces can block the attacker, capture the attacker,
-    // or whether king has legal moves
-    bool isCheckmated{ isKingInCheck(board, kingColor) && !doesKingHaveValidMoves(board, kingColor) };
+    bool isCheckmated{ isKingInCheck(board, kingColor) && !playerHasLegalMoves(board, kingColor) };
 
     return isCheckmated;
 }
