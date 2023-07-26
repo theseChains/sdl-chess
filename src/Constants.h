f6a6b31bf36da7447e3b8c7861f6ef1bc1d2ae10
theseChains <rasim0103@gmail.com>
2023-07-26 23:29:03 +0300

    Add constants for position conversion

--- src/Constants.h
+++ src/Constants.h
@@ -1,15 +1,29 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#include "DrawColor.h"
+
 namespace constants
 {
     inline constexpr int windowWidth{ 800 };
     inline constexpr int windowHeight{ 800 };
     inline constexpr int boardSize{ 8 };
+    inline constexpr int screenPositionMultiplier{ 100 };
 
-    // rows and columns go from 0 to 7
+    inline constexpr int tileWidth{ windowWidth / boardSize };
+    inline constexpr int tileHeight{ windowHeight / boardSize };
+
     inline constexpr int whitePawnStartRow{ 6 };
     inline constexpr int blackPawnStartRow{ 1 };
+    inline constexpr int topRow{ 0 };
+    inline constexpr int bottomRow{ 7 };
+}
+
+namespace colors
+{
+    inline constexpr DrawColor white{ 255, 255, 255, 255 };
+    inline constexpr DrawColor lightTile{ 230, 230, 230, 255 };
+    inline constexpr DrawColor darkTile{ 150, 75, 0, 255 };
 }
 
 #endif
