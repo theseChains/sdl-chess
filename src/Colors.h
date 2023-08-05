171bb10eee23049fbd476907fe465a4fe329c99a
theseChains <rasim0103@gmail.com>
2023-08-05 14:53:14 +0300

    Add the color green

--- src/Colors.h
+++ src/Colors.h
@@ -20,6 +20,8 @@ enum class PieceColor
 namespace colors
 {
     inline constexpr DrawColor white{ 255, 255, 255, 255 };
+    inline constexpr DrawColor green{ 50, 205, 50, 255 };
+
     inline constexpr DrawColor lightTile{ 230, 230, 230, 255 };
     inline constexpr DrawColor darkTile{ 150, 75, 0, 255 };
 }
