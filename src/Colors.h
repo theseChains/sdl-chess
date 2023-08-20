92e341bea3d21fcc8a0ad4836f62337633bebb50
theseChains <rasim0103@gmail.com>
2023-08-21 01:18:49 +0300

    Add the color red

--- src/Colors.h
+++ src/Colors.h
@@ -21,6 +21,7 @@ namespace colors
 {
     inline constexpr DrawColor white{ 255, 255, 255, 255 };
     inline constexpr DrawColor green{ 50, 205, 50, 255 };
+    inline constexpr DrawColor red{ 220, 20, 60, 255 };
 
     inline constexpr DrawColor lightTile{ 230, 230, 230, 255 };
     inline constexpr DrawColor darkTile{ 150, 75, 0, 255 };
